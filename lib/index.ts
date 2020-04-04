e876c3cf3c7092b3fd7a897954d674281ed14eac
Carlos Morales <clmorales@nd-soft.com>
2020-04-04 00:59:02 -0400

    Initial release

---
+++ lib/index.ts
@@ -0,0 +1,21 @@
+
+export declare class MyModuleService {
+  /**
+     * Manual set for subscription ID
+     */
+    subscriptionId: string;
+    /**
+     * Manual set for organization ID
+     */
+    organizationId: string;
+    /**
+     * Manual set for facility ID
+     */
+    facilityId: string;
+    /**
+     * Manual set for patient ID
+     */
+    patientId: string;
+ 
+    constructor()
+}
